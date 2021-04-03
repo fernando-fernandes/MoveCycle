874d456786f6de16df029e254d6e21aba330d473
Fernando <fernando.85@outlook.com>
2021-04-03 03:53:36 -0300

    Componente de Perfil

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from '../styles/Pages/Home.module.css';
 
 
 export default function Home() {
-  return (
-    <div className="container">
-      <ExperienceBar />
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <ExperienceBar />
+
+            <section>
+                <div>
+                    <Profile />
+                </div>
+
+                <div>
+
+                </div>
+            </section>
+        </div>
+    )
 }
