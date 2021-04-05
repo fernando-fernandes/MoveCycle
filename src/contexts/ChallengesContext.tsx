9eba102e2053aab0396362e7deb0f7e20c0454d4
Fernando <fernando.85@outlook.com>
2021-04-05 00:24:17 -0300

    Notificação

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import challenges from '../../challenges.json';
 
 
@@ -38,6 +38,10 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+    useEffect( () => {
+        Notification.requestPermission()
+;    }, []);
+
     function levelUp() {
         setLevel(level + 1);
     }
@@ -47,6 +51,15 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
         const challenge = challenges[randomChallengeindex];
 
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        if(Notification.permission === 'granted'){
+            new Notification('Novo desafio! 🎉 ',
+            {
+                body: `Valendo ${challenge.amount}xp"`
+            })
+        }
     }
 
     function resetChallenge(){
